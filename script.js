8aa64912f0e12a52eab10da5a2700f14949b3b17
Elisa Elezi <elisaelezi12@gmail.com>
2023-08-25 01:11:42 -0400

    Starting this all over and using a video for help and guidance

--- script.js
+++
@@ -1,8 +0,0 @@
-const containerEl = document.querySelector('.container');
-
-for(i = 0; i < 16; i++) {
-    let board = document.createElement('div');
-    board.className = 'boxes';
-
-    document.getElementById('board').appendChild(board);
-}
